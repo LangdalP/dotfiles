c5d8af7682809c6e9dc1684d342db2fb1ddf6ee1
Peder Voldnes Langdal <peder.github@langd.al>
2024-01-10 12:29:50 +0100

    Added trim-nvim plugin for nvim

---
+++ nvim/lua/plugins/trim-nvim.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "cappyzawa/trim.nvim",
+    opts = {
+      trim_on_write = true,
+      trim_trailing = true,
+      trim_last_line = false,
+      trim_first_line = false,
+    },
+  },
+}
