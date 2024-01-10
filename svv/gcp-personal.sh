96710791b652139d951dbbc206a5afb5fe9e7396
Peder Voldnes Langdal <peder.github@langd.al>
2024-01-10 21:24:44 +0100

    Remove unused SVV stuff, add better stuff

--- svv/gcp-personal.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-
-# Edit this to your own account
-gcloud config set account peder.voldnes.langdal@vegvesen.no
-unset GOOGLE_APPLICATION_CREDENTIALS
-
-echo "peder.voldnes.langdal@vegvesen.no activated in gcloud and GOOGLE_APPLICATION_CREDENTIALS has been unset"
-
