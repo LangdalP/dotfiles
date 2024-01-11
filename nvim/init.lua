24bc4931abe6f0ed2b2d388884be9c9339ad48cb
Peder Voldnes Langdal <peder.github@langd.al>
2024-01-11 22:22:26 +0100

    Add sql-formatter support to nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,15 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+require("conform").formatters["sql-formatter"] = {
+  command = "sql-formatter",
+  stdin = true,
+}
+
+require("conform").formatters_by_ft = {
+  sql = { "sql-formatter" },
+}
+
 -- require("pets").setup({
 --   -- your options here
 -- })
