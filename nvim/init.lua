f186b0c505783bff568e97acfb8009cfdfd71146
Peder Voldnes Langdal <peder.github@langd.al>
2024-02-03 11:00:04 +0100

    Added nvim keymap for copy/paste in Neovide

--- nvim/init.lua
+++ nvim/init.lua
@@ -10,6 +10,14 @@ require("conform").formatters_by_ft = {
   sql = { "sql-formatter" },
 }
 
+-- Allow clipboard copy paste in neovim - needed when using neovide
+vim.g.neovide_input_use_logo = 1
+vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true })
+
 -- require("pets").setup({
 --   -- your options here
 -- })
+--
